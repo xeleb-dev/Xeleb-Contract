a340fb62f3c8bc2f2f3264856f86b76d243771d3
Kaito <kaito@192.168.0.104>
2025-07-14 10:28:42 +0700

    remove burn feature in tokenERC20

--- contracts/MissAIerc20.sol
+++ contracts/MissAIerc20.sol
@@ -3,10 +3,9 @@
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract MissAIerc20 is ERC20, ERC20Burnable, Ownable {
+contract TokenERC20 is ERC20, Ownable {
     bool public isBonding;
     address public bondingCurve;
 
